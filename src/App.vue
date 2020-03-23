b85a0660dec6f78e9edcce2dd4afaa4d447c2eb6
dakeng <123maple_leaves@sina.com>
2020-03-24 02:58:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  height: 100%;
+  // background-color: #2f5573;
+}
+body {
+  margin: 0;
+  height: 100%;
+  display: flex;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  // background-color: #2f5573;
+}
+</style>
