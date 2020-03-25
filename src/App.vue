6064f1b3c5155e55fd222f027a2cc07f5b8438ec
dakeng <123maple_leaves@sina.com>
2020-03-26 01:46:09 +0800

    add task list

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,41 @@
         </div>
       </div>
     </div>
+
+    <div class="content-container">
+      <div class="side-container">
+        <div class="item-container user-container">
+          <i class="iconfont icon-user"></i>
+          <span>User</span>
+        </div>
+        <div class="task-container">
+          <div class="item-container task-list-title">
+            <i class="iconfont icon-star"></i>
+            <span>今天</span>
+            <span></span>
+          </div>
+          <div class="item-container task-list-title">
+            <i class="iconfont icon-menu"></i>
+            <span>明天</span>
+            <span></span>
+          </div>
+          <div class="item-container task-list-title">
+            <i class="iconfont icon-calendar"></i>
+            <span>日历</span>
+            <span></span>
+          </div>
+          <i class="line"></i>
+        </div>
+        <div class="item-container task-list-title">
+          <i class="iconfont icon-plus"></i>
+          <span>新建清单</span>
+          <span></span>
+        </div>
+      </div>
+      <div class="main-container">
+      
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,7 +61,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import '//at.alicdn.com/t/font_1712277_nf4b7m31uva.css';
+@import '//at.alicdn.com/t/font_1712277_w7o4lsqwkqr.css';
 
 html {
   height: 100%;
@@ -53,10 +88,16 @@ body {
 
 .title-bar {
   -webkit-app-region: drag;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: 999;
+
   height: 32px;
   line-height: 32px;
   color: #fff;
-  background: rgba($color: #fff, $alpha: .2);
+  // background: rgba($color: #fff, $alpha: .2);
 
   .title-bar-container {
     display: flex;
@@ -78,9 +119,62 @@ body {
       user-select: none;
 
       &:hover {
-        background: rgba($color: #fff, $alpha: .25);
+        background: rgba($color: #fff, $alpha: .1);
       }
     }
+  }
+}
+
+.content-container {
+  height: 100%;
+  display: flex;
+
+  .side-container, .main-container {
+    padding-top: 48px;
+  }
+
+  .side-container {
+    flex: 0 0 320px;
+  }
+
+  .main-container {
+    flex: 1 1 auto;
+    background: #26445c;
+  }
+}
+
+.side-container {
+  color: #ccc;
+
+  .item-container {
+    padding: 0 12px;
+
+    display: flex;
+
+    i, span {
+      line-height: 48px;
+      vertical-align: middle;
+      cursor: default;
+    }
+
+    i {
+      padding: 0 12px;
+    }
+
+    &:hover {
+      background: rgba($color: #fff, $alpha: .1);
+    }
+  }
+
+  .line {
+    display: block;
+    margin: 12px 24px;
+    height: 1px;
+    background: rgba($color: #fff, $alpha: .2);
+  }
+
+  .task-list-title {
+
   }
 }
 </style>
